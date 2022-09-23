ff6e07842c90513a0f7918a2846f7c63509c75dc
Kh Asad Mukhtar <kmukhtar.bscs18seecs@seecs.edu.pk>
2022-09-23 07:38:16 +0500

    first commit

---
+++ src/Components/DigitsButton.js
@@ -0,0 +1,9 @@
+import { ACTIONS } from "../App";
+
+function DigitButton({dispatch,digit}){
+    return (
+        <button onClick={()=>dispatch({type:ACTIONS.ADD_DIGITS,payload:{digit}})}>{digit}</button>
+    )
+}
+
+export default DigitButton;
